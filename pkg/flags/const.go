4f9e1a49dc7410a0dcc4d6a2f31cc244892b23d2
Saurabh Deoras <sdeoras@gmail.com>
2022-03-01 09:33:53 -0800

    added gen command
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

---
+++ pkg/flags/const.go
@@ -0,0 +1,8 @@
+package flags
+
+const (
+	DerivationPath         = "derivation-path"
+	UsePassphrase          = "use-passphrase"
+	SkipMnemonicValidation = "skip-mnemonic-validation"
+	InputHexSeed           = "input-hex-seed"
+)
